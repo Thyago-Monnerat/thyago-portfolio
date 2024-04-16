62eb0778640599371472cc9004b840435b074dc1
Thyago Monnerat <101063531+Thyago-Monnerat@users.noreply.github.com>
2024-04-16 15:33:09 -0300

    Update intro.tsx

--- portfolio_page/src/components/intro.tsx
+++ portfolio_page/src/components/intro.tsx
@@ -22,7 +22,7 @@ const Intro: React.FC = () =>{
       </div>
     
       <div className="w-full flex justify-center gap-[9px] lg:justify-start">
-       <DefaultButton href="https://www.linkedin.com/ambry/?x-li-ambry-ep=AQKmdsybQUn5BgAAAY5Tsep3tTMEJfICxSdj9XUvxij87da-dYWjs0qqNTNwwsVscsrCKG6Gd1d-AbNrtEC3iJcNyt3LM9jGTjRfA4bEEgJZPBBAYH0XNaWeNLgjcofY9eLqcbzg-Xpq2bFe6XDT-mnDcuKda7O8Nb_BESWPLucYRehEXa1Nu1wwm5O_D0r96HQx3tuvIx3LgKJOCdztm0CCAWSUGDytEl6OWnJXken1Z3xG6skzP_4cEUSSuX2Qik688WGlSUCKbfG4Fc4b7rTUWFMa8h50MZiR4sXgkN36ynkeCuh7I8KrX2mh2uHG6qWE44OSamo3v6MW9utflOTzFCfoD4IVYI3PyzqKCREpJONa4n9Pb7r6PCSi0v02BlIlv3p7fpsu7vbtRoHAby0cX2_re0_hi_uZrx7_LXbjmECgF744mtqv2Rlbh5ryZrUYcMBGohtjd3fIzAYLM82Gf5Yg3g8MPw33akNXxXyeTzGF1PKwCGt5GF9DZytM60PV5Im1752eo64F1fFuyzA-5tj3LovFoCmh5pW3JnzqjmmDCyD55QeJCs-gJB8JDgP6-A&x-ambry-um-filename=Profile.pdf" content="Download CV" props="bg-main-green hover:bg-[#18f373] transition duration-[.3s] ease"></DefaultButton>
+       <DefaultButton href="src/assets/THYAGOLOPESMONNERAT.pdf" doownload="THYAGOLOPESMONNERAT.pdf" content="Download CV" props="bg-main-green hover:bg-[#18f373] transition duration-[.3s] ease"></DefaultButton>
         <DefaultButton href="mailto:thyago.monnerat.pro@gmail.com" content="Entrar em contato" props="text-white border border-1 border-solid border-color-button transition-border duration-[.3s] ease hover:border-white" ></DefaultButton>
       </div>
    </div>
