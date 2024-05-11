089ba817bf4bd2a920d426586878da59183fd052
Thyago <thy_thyago2.5@hotmail.com>
2024-05-10 21:14:48 -0300

    -Remoção do arquivo tsx HandleDownload

--- portfolio_page/src/components/handleDownload.tsx
+++
@@ -1,9 +0,0 @@
-import { saveAs } from 'file-saver';
-
-const handleDownload = () => {
-    // Caminho do arquivo no seu projeto
-    const filePath = '/assets/cs-thyagoMonnerat.pdf';
-    saveAs(filePath, 'meu_portfolio.pdf');
-};
-
-export default handleDownload;
